88d8dc13b6c1ea978faa523da4b7e1243d69e528
Steve Manuel <stevenhmanuel@gmail.com>
2014-09-01 13:23:25 -0700

    new logging system for files served

--- file-server.go
+++ file-server.go
@@ -6,16 +6,59 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/nilslice/color"
 )
 
+// LoggedRequest is a type that abstracts the file requested and information
+// about it to be logged to the console.
+type LoggedRequest struct {
+	timestamp string
+	method    string
+	path      string
+	protocol  string
+}
+
+// NewLoggedRequest creates and returns a correctly formed LoggedRequest
+func NewLoggedRequest(req *http.Request) *LoggedRequest {
+	t := time.Now().Format(time.StampMilli)
+	lr := &LoggedRequest{
+		timestamp: t,
+		method:    req.Method,
+		path:      req.URL.Path,
+		protocol:  req.Proto,
+	}
+	return lr
+}
+
+// Log prints the contents of LoggedRequest to the console in a developer friendly
+// format. Implement colors for better readability.
+func (lr *LoggedRequest) Log() {
+	t := "|" + lr.timestamp + "|"
+	p := "(" + lr.protocol + ")"
+	fmt.Println(
+		t,
+		color.CyanString(p),
+		color.CyanString(lr.method),
+		color.YellowString(lr.path),
+	)
+}
+
 var (
-	port *int    = flag.Int("port", 5000, "port to open for incoming requests.")
-	addr *string = flag.String("addr", "127.0.0.1", "address to bind the server")
+	port = flag.Int("port", 5000, "port to listen on for incoming requests")
+	addr = flag.String("addr", "127.0.0.1", "address to bind the server")
 )
 
 func exitFromError(err error) {
 	fmt.Errorf("Error: %v\nExiting.", err)
 	os.Exit(1)
+}
+
+func combinedLogAndFileServer(res http.ResponseWriter, req *http.Request, dir string) {
+	lr := NewLoggedRequest(req)
+	lr.Log()
+	http.ServeFile(res, req, dir+req.URL.Path)
 }
 
 func main() {
@@ -26,7 +69,9 @@ func main() {
 		exitFromError(err)
 	}
 
-	http.Handle("/", http.FileServer(http.Dir(directory)))
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		combinedLogAndFileServer(res, req, directory)
+	})
 
 	serverAddress := net.JoinHostPort(*addr, fmt.Sprint(*port))
 
